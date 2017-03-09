e15bdf4f601870c956789f3cd56ebbb89c30ea48
raphaelduhs <raphael.duhs@gmail.com>
2017-03-09 16:55:45 +0100

    Update 1.0 hugo is on its way

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,8 @@ package com.example.raphaelduhs.hugo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setName (View view ) {
+        EditText textField = (EditText) findViewById(R.id.editText);
+
+
+        textField.setText("HUgo");
+
+
     }
 }
